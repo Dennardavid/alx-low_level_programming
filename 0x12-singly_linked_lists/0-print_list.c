8e7f41e3134bad1928cb0dfff7309f8be531e409
Dennar David <dennardavid@gmail.com>
2023-03-31 16:43:22 -0700

    first task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,15 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-
-	for (; h; h = h->next, ++nodes) {
-		if (h->str)
-			printf("[%d] %s\n", h->len, h->str);
+	size_t elementCount = 0;
+	while (h != NULL)
+	{
+		if (h -> str == NULL)
+			printf("[0] (nil)\n";
 		else
-			printf("[0] (nil)\n");
+			printf("[%lu] %s\n", (unsigned long) h->len, h->str);
+			h =h->next;
 	}
-
-	return nodes;
+	
+	return (elementCount);
 }
