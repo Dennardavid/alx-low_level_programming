03cecc81c880968bc736b41a7a1199f3cd487470
Dennar David <dennardavid@gmail.com>
2023-04-10 16:13:38 -0700

    task two

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,15 +2,14 @@
 
 /**
  * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
+ * @n: The number is  to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-
-	if (n & 1)
-       		 _putchar('1');
-   	else
-       		 _putchar('0');
+	// Print the rightmost digit of the binary representation of the number.
+    // This is done by checking if the rightmost digit is 0 or 1 using a binary "AND" operation.
+    // Then the result is added to the character '0' to get the ASCII code of the digit, and printed to the console
+	_putchar((n & 1) + '0');
 }
