00bbfa0bc60be6387ffe16b804489e18e05f8b9d
Dennar David <dennardavid@gmail.com>
2023-04-10 16:19:35 -0700

    task Three

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,9 +7,7 @@
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-		print_binary(n >> 1);
-	// Print the rightmost digit of the binary representation of the number.
-    // This is done by checking if the rightmost digit is 0 or 1 using a binary "AND" operation.
-    // Then the result is added to the character '0' to get the ASCII code of the digit, and printed to the console
+		print_binary(n >> 1)
+
 	_putchar((n & 1) + '0');
 }
