d6b579b46b37e3de042caa9691b134729bc45f03
Dennar David <dennardavid@gmail.com>
2023-04-11 04:42:28 -0700

    task one

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t open_file, read_file, write_file;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
@@ -22,18 +22,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	open_file = open(filename, O_RDONLY);
-	read_file = read(open_file, buffer, letters);
-	write_file = write(STDOUT_FILENO, buffer, read_filr);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (open_file == -1 || read_file == -1 || write_file == -1 || write_file != read_file)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(open_file);
+	close(o);
 
-	return (write_file);
+	return (w);
 }
